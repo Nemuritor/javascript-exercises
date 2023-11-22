f08248e05be1bebb81b2a6aeef51e7dc0c7204d1
Nemuritor <12960064+Nemuritor@users.noreply.github.com>
2023-11-22 17:55:03 -0500

    completed lesson 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemperature) {
+  let roundedTemperature = (fTemperature - 32) * 5 / 9;
+  return Math.round(roundedTemperature * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemperature) {
+  let roundedTemperature = (cTemperature * 9 / 5 + 32);
+  return Math.round(roundedTemperature * 10) / 10;
 };
 
 // Do not edit below this line
